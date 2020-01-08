8fe1aef26eb60ace16a489ff8612d787faebdd16
Ivan Pavlyk <bernero789@gmail.com>
2020-01-08 14:02:47 +0200

    edit db configuration for connecting to the db from host

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-docker build -t postgres ./postgres
+docker build --build-arg "DOCKER_HOST=$(ip -4 addr show docker0 | grep -Po 'inet \K[\d.]+')" -t postgres ./postgres 
 docker rm -f postgres &> /dev/null
 docker run -dit --rm -p 5432:5432 --name postgres postgres
